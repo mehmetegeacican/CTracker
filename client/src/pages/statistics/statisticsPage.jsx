3018d2acf66f9319f6960e0e7e0b47a605685806
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2025-01-09 19:13:53 +0300

    statistics page update

--- client/src/pages/statistics/statisticsPage.jsx
+++ client/src/pages/statistics/statisticsPage.jsx
@@ -1,13 +1,28 @@
-import { Flex } from 'antd'
+import { Flex, Tabs } from 'antd'
 import React from 'react'
 import CaseTable from '../../components/tables/caseTable'
 
 export default function StatisticsPage() {
+  const tabItems = [
+    {
+      key: '1',
+      label: 'Graph View',
+      children: <div>Graph View</div>,
+    },
+    {
+      key: '2',
+      label: 'Table View',
+      children: <CaseTable/>,
+    },
+  ]
   return (
     <div>
       <h1>Statistics Section</h1>
+      <Flex>
+        <div>Filtering Section</div>
+      </Flex>
       <Flex vertical gap={20}>
-          <CaseTable/>
+          <Tabs defaultActiveKey='1' items={tabItems}></Tabs>
       </Flex>
     </div>
   )
