4195a96a2107266f621f0b5cf31ab65d0f0c32d8
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2025-01-09 16:54:57 +0300

    report table init

--- client/src/pages/reports/reportsPage.jsx
+++ client/src/pages/reports/reportsPage.jsx
@@ -1,12 +1,14 @@
-import { Button, Flex } from 'antd'
+import { Button, Divider, Flex } from 'antd'
 import React from 'react'
 import ReportTable from '../../components/tables/reportTable/reportTable'
 
 export default function ReportsPage() {
   return (
     <div>
-      <h1>Reports Page</h1>
-      <p>This is the reports page content.</p>
+      <h1>Reports Section</h1>
+      <Divider/>
+      <p>You can view the reports here in table format.</p>
+      <p>You can add new report from here as well</p>
       <Flex vertical gap={20}>
           <Flex justify='flex-end'>
               <Button>Add New</Button>
