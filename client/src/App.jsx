57af42717357e8b25315488280b7177ef313e369
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2025-01-09 15:54:05 +0300

    react initizalized

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <>
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>The Main Page</h1>
+      <Button type="primary">Click Me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
