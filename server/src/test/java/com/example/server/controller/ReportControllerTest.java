11a17a79bcb3df21f5d1e8ca271ea914bc43f786
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2025-01-10 19:00:25 +0300

    controller tests init

---
+++ server/src/test/java/com/example/server/controller/ReportControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.server.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportControllerTest {
+
+    @Test
+    void getAllReports() {
+    }
+
+    @Test
+    void getSpecificReport() {
+    }
+
+    @Test
+    void postNewReport() {
+    }
+
+    @Test
+    void deleteReport() {
+    }
+}
